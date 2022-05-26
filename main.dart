b16971525c4a761f17231b28f38c7e1518cb74d5
Laiba Riaz <99052447+Lybahshah@users.noreply.github.com>
2022-05-26 07:50:50 -0800

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GridView Custom',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'GridView Custom'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+   @override
+  Widget build(BuildContext context) {
+	return Scaffold(
+	  body: GridView.count(
+		crossAxisCount: 3,
+  children: <Widget>[
+	Container(
+	  padding: const EdgeInsets.all(8),
+	  child: Column(children: <Widget> [
+                   Image.asset('assets/images/1.jpg'),
+	          const Text('Lion',style: TextStyle(backgroundColor: Color.fromARGB(255, 221, 214, 214)),)
+                       ],
+                )
+	),
+  Container(
+	  padding: const EdgeInsets.all(8),
+	  child: Column(children: <Widget> [
+                   Image.asset('assets/images/2.jpg'),
+	          const Text('Tiger',style: TextStyle(backgroundColor: Color.fromARGB(255, 221, 214, 214)),)
+                       ],
+                )
+	),
+  Container(
+	  padding: const EdgeInsets.all(8),
+	  child: Column(children: <Widget> [
+                   Image.asset('assets/images/3.jpg'),
+	          const Text('Elephant',style: TextStyle(backgroundColor: Color.fromARGB(255, 221, 214, 214)),)
+                       ],
+                )
+	),
+  Container(
+	  padding: const EdgeInsets.all(8),
+	  child: Column(children: <Widget> [
+                   Image.asset('assets/images/4.jpg'),
+	          const Text('Monkey',style: TextStyle(backgroundColor: Color.fromARGB(255, 221, 214, 214)),)
+                       ],
+                )
+	),
+  Container(
+	  padding: const EdgeInsets.all(8),
+	  child: Column(children: <Widget> [
+                   Image.asset('assets/images/5.jpg'),
+	          const Text('Zebra',style: TextStyle(backgroundColor: Color.fromARGB(255, 221, 214, 214)),)
+                       ],
+                )
+	),
+  Container(
+	  padding: const EdgeInsets.all(8),
+	  child: Column(children: <Widget> [
+                   Image.asset('assets/images/6.jpg'),
+	          const Text('Giraffe',style: TextStyle(backgroundColor: Color.fromARGB(255, 221, 214, 214)),)
+                       ],
+                )
+	),
+  Container(
+	  padding: const EdgeInsets.all(8),
+	  child: Column(children: <Widget> [
+                   Image.asset('assets/images/7.jpg'),
+	          const Text('Cheetah',style: TextStyle(backgroundColor: Color.fromARGB(255, 221, 214, 214)),)
+                       ],
+                )
+	),
+  Container(
+	  padding: const EdgeInsets.all(8),
+	  child: Column(children: <Widget> [
+                   Image.asset('assets/images/8.jpg'),
+	          const Text('Leopard',style: TextStyle(backgroundColor: Color.fromARGB(255, 221, 214, 214)),)
+                       ],
+                )
+	),
+  Container(
+	  padding: const EdgeInsets.all(8),
+	  child: Column(children: <Widget> [
+                   Image.asset('assets/images/9.jpg'),
+	          const Text('Deer',style: TextStyle(backgroundColor: Color.fromARGB(255, 221, 214, 214)),)
+                       ],
+                )
+	),
+  Container(
+	  padding: const EdgeInsets.all(8),
+	  child: Column(children: <Widget> [
+                   Image.asset('assets/images/10.jpg'),
+	          const Text('Wolf',style: TextStyle(backgroundColor: Color.fromARGB(255, 221, 214, 214)),)
+                       ],
+                )
+	),
+  Container(
+	  padding: const EdgeInsets.all(8),
+	  child: Column(children: <Widget> [
+                   Image.asset('assets/images/11.jpg'),
+	          const Text('Bear',style: TextStyle(backgroundColor: Color.fromARGB(255, 221, 214, 214)),)
+                       ],
+                )
+	),
+  Container(
+	  padding: const EdgeInsets.all(8),
+	  child: Column(children: <Widget> [
+                   Image.asset('assets/images/12.jpg'),
+	          const Text('Fox',style: TextStyle(backgroundColor: Color.fromARGB(255, 221, 214, 214)),)
+                       ],
+                )
+	),
+  ],
+)
+	);
+  }
+}
